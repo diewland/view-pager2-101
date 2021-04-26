afee530fb76ca1b6236db6b24921ebc0212424a3
diewland <diewland@gmail.com>
2021-04-26 17:51:43 +0700

    add basic adapter

---
+++ app/src/main/java/com/example/viewpager2101/BasicAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.viewpager2101
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+
+class BasicAdapter(context: Context, tvList: ArrayList<TextView>, viewPager2: ViewPager2):
+    RecyclerView.Adapter<BasicAdapter.ViewHolder>() {
+
+    private val mInflater: LayoutInflater
+    private val data: ArrayList<TextView>
+    private val viewPager2: ViewPager2
+
+    init {
+        mInflater = LayoutInflater.from(context)
+        data = tvList
+        this.viewPager2 = viewPager2
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = mInflater.inflate(R.layout.item_blank, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val t = data.get(position)
+        holder.container.addView(t)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class ViewHolder internal constructor(itemView: View):
+        RecyclerView.ViewHolder(itemView) {
+
+        var container: LinearLayout
+
+        init {
+            container = itemView.findViewById(R.id.blank_container)
+            /*
+            myTextView = itemView.findViewById(R.id.tvTitle)
+            relativeLayout = itemView.findViewById(R.id.container)
+            button = itemView.findViewById(R.id.btnToggle)
+            button.setOnClickListener {
+                viewPager2.orientation = when(viewPager2.orientation) {
+                    ViewPager2.ORIENTATION_HORIZONTAL -> ViewPager2.ORIENTATION_VERTICAL
+                    else -> ViewPager2.ORIENTATION_HORIZONTAL
+                }
+            }
+            */
+        }
+    }
+
+}
--- app/src/main/java/com/example/viewpager2101/MainActivity.kt
+++ app/src/main/java/com/example/viewpager2101/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.viewpager2101
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +22,14 @@ class MainActivity : AppCompatActivity() {
             "Fourth Screen",
         )
 
-        viewPager2.adapter = ViewPagerAdapter(this, list, viewPager2)
+        val tvList = arrayListOf<TextView>()
+        list.forEach {
+            val t = TextView(this)
+            t.text = it
+            tvList.add(t)
+        }
+
+        // viewPager2.adapter = ViewPagerAdapter(this, list, viewPager2)
+        viewPager2.adapter = BasicAdapter(this, tvList, viewPager2)
     }
 }
