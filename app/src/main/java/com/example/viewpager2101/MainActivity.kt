ec7b1a5d02530d8e84b3c0a48f94bb3dbf03adf2
diewland <diewland@gmail.com>
2021-04-27 16:59:38 +0700

    add update data function

--- app/src/main/java/com/example/viewpager2101/MainActivity.kt
+++ app/src/main/java/com/example/viewpager2101/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.viewpager2101
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
@@ -8,12 +9,16 @@ import androidx.viewpager2.widget.ViewPager2
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewPager2: ViewPager2
+    lateinit var btn1: Button
+    lateinit var btn2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewPager2 = findViewById(R.id.viewPager2)
+        btn1 = findViewById(R.id.btn_1)
+        btn2 = findViewById(R.id.btn_2)
 
         val list = arrayListOf(
             "First Screen",
@@ -22,14 +27,36 @@ class MainActivity : AppCompatActivity() {
             "Fourth Screen",
         )
 
+        var tvList = buildList(list)
+
+        // viewPager2.adapter = ViewPagerAdapter(this, list, viewPager2)
+        // viewPager2.adapter = BasicAdapter(this, tvList, viewPager2)
+        updateData(tvList)
+
+        // bind buttons
+        btn1.setOnClickListener {
+            updateData("one", "two", "three")
+        }
+        btn2.setOnClickListener {
+            updateData("apple", "microsoft", "google")
+        }
+    }
+
+
+    fun updateData(l: ArrayList<TextView>) {
+        viewPager2.adapter = BasicAdapter(this, l, viewPager2)
+    }
+    fun updateData(vararg l: String) {
+        val al = buildList(l.map { it })
+        viewPager2.adapter = BasicAdapter(this, al, viewPager2)
+    }
+    fun buildList(t: List<String>): ArrayList<TextView> {
         val tvList = arrayListOf<TextView>()
-        list.forEach {
+        t.forEach {
             val t = TextView(this)
             t.text = it
             tvList.add(t)
         }
-
-        // viewPager2.adapter = ViewPagerAdapter(this, list, viewPager2)
-        viewPager2.adapter = BasicAdapter(this, tvList, viewPager2)
+        return tvList
     }
 }
